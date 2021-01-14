cf323f38c545bc2391c04a8b84afb809c96ba680
rohitsandhu <xxmdluffxx@gmail.com>
2021-01-14 11:40:11 +0100

    abans de crear partides, rondes i torneig

--- src/Form/ArbitreType.php
+++ src/Form/ArbitreType.php
@@ -2,34 +2,29 @@
 
 namespace App\Form;
 
-use App\Entity\Jugador;
+use App\Entity\Arbitre;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 
-class JugadorType extends AbstractType
+class ArbitreType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('nom')
             ->add('cognoms')
-            ->add('elo')
             ->add('pais')
-            ->add('club')
+            ->add('usuari')
+            ->add('contrasenya')
             ->add('dni')
-            ->add('dataNaixament', DateType::class, [
-                'years'  =>range(1900, 2021),
-            ])
-
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Jugador::class,
+            'data_class' => Arbitre::class,
         ]);
     }
 }
